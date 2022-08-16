baf1d72e1e7044ffc4f086bfeeb23357ac68c3a1
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-16 22:12:25 +0000

    create getTaskDetail on the repository layer

--- src/tasks/tasks-repositories.ts
+++ src/tasks/tasks-repositories.ts
@@ -3,4 +3,13 @@ import Task from "./tasks-entities";
 
 const repository = AppDataSource.getRepository(Task);
 
+export const getTaskDetail = async (taskID: number) => {
+  const task = await repository.findOne({
+    order: { id: "DESC" },
+    where: { user: { id: taskID } },
+  });
+
+  return task;
+};
+
 export default repository;
